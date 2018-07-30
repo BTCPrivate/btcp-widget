9135d948787a1d4fa3224ddef198c882647cc371
Matt Pass <matt@mattpass.com>
2018-07-30 19:33:46 +0100

    Prefixing, standardizing and sanitizing

--- plugins/btcp-pay-woocommerce.php
+++ plugins/btcp-pay-woocommerce.php
@@ -1,7 +1,7 @@
 <?php
 /*
-Plugin Name: Bitcoin Private for Woocommerce
-Plugin URI: http://wordpress.org/plugins/btcp-pay-woocommerce/
+Plugin Name: BTCP Pay for WooCommerce
+Plugin URI: http://wordpress.org/plugins/btcp-pay-for-woocommerce/
 Description: Official BTCP Pay plugin from the Bitcoin Private core dev team. Allows users to pay on the WooCommerce ecommerce system so merchants can take Bitcoin Private payments
 Author: Bitcoin Private (J62 & MattPass)
 Version: 1.0
@@ -14,7 +14,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Function to change format of prices shown
-function addPriceSuffix() {
+function btcp_widget_add_price_suffix() {
   $currency = "BTCP";
   $format = '%2$s&nbsp;' . $currency;
 
@@ -22,16 +22,16 @@ function addPriceSuffix() {
 }
 
 // Pickup widget code, split into array pieces and set a flag that for now we haven't determined user has set "currency" : BTCP"
-$widgetData = str_replace("\r","",get_option('btcp_pay_woocommerce_widget_code'));
-$widgetData = explode("\n",$widgetData);
-$btcpWidgetCurrencySet = false;
+$btcp_widget_data = str_replace("\r","",get_option('btcp_woocommerce_widget_code'));
+$btcp_widget_data = explode("\n",$btcp_widget_data);
+$btcp_widget_currency_set = false;
 // For each line in widget data
-for($i=0; $i<count($widgetData); $i++) {
+for($i=0; $i<count($btcp_widget_data); $i++) {
   // If we've found the currency line and the value is "BTCP"
-  if (strpos($widgetData[$i], '"currency"') > -1 && explode('"',$widgetData[$i])[3] === "BTCP") {
+  if (strpos($btcp_widget_data[$i], '"currency"') > -1 && explode('"',$btcp_widget_data[$i])[3] === "BTCP") {
     // User has set currency, replace all prices shown into the format
-    $btcpWidgetCurrencySet = true;
-    add_action('woocommerce_price_format', 'addPriceSuffix');
+    $btcp_widget_currency_set = true;
+    add_action('woocommerce_price_format', 'btcp_widget_add_price_suffix');
     ?>
     <script>
       // Update cart total, as doesn't seem to update using above, this is an override
@@ -53,20 +53,20 @@ for($i=0; $i<count($widgetData); $i++) {
 }
 
 // Go to settings page after activation
-function btcp_pay_woocommerce_activate( $plugin ) {
-    if( $plugin == plugin_basename( __FILE__ ) ) {
+function btcp_woocommerce_activate($plugin) {
+    if($plugin == plugin_basename( __FILE__ )) {
         exit(wp_redirect(admin_url('options-general.php?page=btcp-pay-woocommerce')));
     }
 }
-add_action('activated_plugin', 'btcp_pay_woocommerce_activate');
+add_action('activated_plugin', 'btcp_woocommerce_activate');
 
 // Add option to admin menu > settings section
 add_action('admin_menu', function() {
-  add_options_page('BTCP Pay WooCommerce Settings', 'BTCP Pay WooCommerce', 'manage_options', 'btcp-pay-woocommerce', 'btcp_pay_woocommerce');
+  add_options_page('BTCP Pay WooCommerce Settings', 'BTCP Pay WooCommerce', 'manage_options', 'btcp-pay-woocommerce', 'btcp_woocommerce_settings');
 });
 
 // Form for user to paste widget code into
-function btcp_pay_woocommerce() {
+function btcp_woocommerce_settings() {
  ?>
    <h1>BTCP Pay WooCommerce Settings</h1>
    <p>Please paste the <strong>btcpWidget.data</strong> section from your widget code from your account on the <a href="https://btcppay.com" target="_blank">btcppay.com</a> site into the box below (as per the example below) and we'll take take of it working within WooCommerce.</p>
@@ -75,8 +75,8 @@ function btcp_pay_woocommerce() {
    <div class="wrap">
      <form action="options.php" method="post">
        <?php
-       settings_fields( 'btcp-pay-woocommerce-settings' );
-       do_settings_sections( 'btcp-pay-woocommerce-settings' );
+       settings_fields('btcp-pay-woocommerce-settings');
+       do_settings_sections('btcp-pay-woocommerce-settings');
        ?>
        <textarea placeholder='Paste the btcpWidget.data JSON from your BTCP Pay widget code here, eg:
 
@@ -94,7 +94,7 @@ btcpWidget.data = {
           "crust"   : "stuffed",
           "pan"     : "thin base"
       }
-  };' name="btcp_pay_woocommerce_widget_code" style="width: 300px; height: 400px"><?php echo esc_attr( get_option('btcp_pay_woocommerce_widget_code') ); ?></textarea>
+  };' name="btcp_woocommerce_widget_code" style="width: 300px; height: 400px"><?php echo esc_attr( get_option('btcp_woocommerce_widget_code') ); ?></textarea>
 
        <?php submit_button(); ?>
      </form>
@@ -103,12 +103,12 @@ btcpWidget.data = {
 }
 // Register that DB setting
 add_action('admin_init', function() {
-  register_setting('btcp-pay-woocommerce-settings', 'btcp_pay_woocommerce_widget_code');
+  register_setting('btcp-pay-woocommerce-settings', 'btcp_woocommerce_widget_code');
 });
 
 // When plugins loaded, run function to init our BTCP Pay plugin
-add_action('plugins_loaded', 'init_custom_gateway_class');
-function init_custom_gateway_class()
+add_action('plugins_loaded', 'btcp_widget_init_custom_gateway');
+function btcp_widget_init_custom_gateway()
 {
 
     class WC_Gateway_Custom extends WC_Payment_Gateway
@@ -259,22 +259,22 @@ function init_custom_gateway_class()
                 var btcpWidget = {};
                 <?php
                 // Pickup widget code and output after replacing "amount" value with cart amount and adding currency (unless user specified BTCP) and hideButton params also
-                $widgetData = str_replace("\r","",get_option('btcp_pay_woocommerce_widget_code'));
-                $widgetData = explode("\n",$widgetData);
-                for($i=0; $i<count($widgetData); $i++) {
-                  global $btcpWidgetCurrencySet;
-                  if (strpos($widgetData[$i], '"amount"') > -1) {
-                    $widgetData[$i] = '    "amount"      : '.(WC()->cart->total).',\n'.
-                                      ($btcpWidgetCurrencySet
+                $btcp_widget_data = str_replace("\r","",get_option('btcp_woocommerce_widget_code'));
+                $btcp_widget_data = explode("\n",$btcp_widget_data);
+                for($i=0; $i<count($btcp_widget_data); $i++) {
+                  global $btcp_widget_currency_set;
+                  if (strpos($btcp_widget_data[$i], '"amount"') > -1) {
+                    $btcp_widget_data[$i] = '    "amount"      : '.(WC()->cart->total).',\n'.
+                                      ($btcp_widget_currency_set
                                         ? ''
                                         : '    "currency"   : "'.get_woocommerce_currency().'",\n'
                                       ).
                                       '    "hideButton"  : true,';
                   }
                 }
-                echo str_replace("\\n","\n",implode("\n",$widgetData));
+                echo str_replace("\\n","\n",implode("\n",$btcp_widget_data));
                 // Hide block overlays, seem to always show due to overriding currency?
-                if ($btcpWidgetCurrencySet) {
+                if ($btcp_widget_currency_set) {
                 ?>
                 </script>
                 <style>
@@ -376,8 +376,8 @@ function init_custom_gateway_class()
     }
 }
 
-add_filter('woocommerce_payment_gateways', 'add_custom_gateway_class');
-function add_custom_gateway_class($methods)
+add_filter('woocommerce_payment_gateways', 'btcp_woocommerce_add_custom_gateway_class');
+function btcp_woocommerce_add_custom_gateway_class($methods)
 {
     $methods[] = 'WC_Gateway_Custom';
     return $methods;
@@ -386,35 +386,34 @@ function add_custom_gateway_class($methods)
 /**
  * Update the order meta with field value
  */
-add_action('woocommerce_checkout_update_order_meta', 'custom_payment_update_order_meta');
-function custom_payment_update_order_meta($order_id)
+add_action('woocommerce_checkout_update_order_meta', 'btcp_woocommerce_custom_payment_update_order_meta');
+function btcp_woocommerce_custom_payment_update_order_meta($order_id)
 {
 
     if ($_POST['payment_method'] != 'bitcoin_private')
         return;
 
-    update_post_meta($order_id, 'paymentAddress', $_POST['i_payment_address']);
-    update_post_meta($order_id, 'paymentTxid', $_POST['i_payment_txid']);
-    update_post_meta($order_id, 'paymentTxref', $_POST['i_payment_txref']);
+    update_post_meta($order_id, 'btcp_payment_address', sanitize_text_field($_POST['i_payment_address']));
+    update_post_meta($order_id, 'btcp_payment_txid', intval($_POST['i_payment_txid']));
+    update_post_meta($order_id, 'btcp_payment_txref', sanitize_text_field($_POST['i_payment_txref']));
 }
 
 /**
  * Display field value on the order edit page
  */
-add_action('woocommerce_admin_order_data_after_billing_address', 'custom_checkout_field_display_admin_order_meta', 10, 1);
-function custom_checkout_field_display_admin_order_meta($order)
+add_action('woocommerce_admin_order_data_after_billing_address', 'btcp_woocommerce_custom_checkout_field_display_admin_order_meta', 10, 1);
+function btcp_woocommerce_custom_checkout_field_display_admin_order_meta($order)
 {
     $method = get_post_meta($order->id, '_payment_method', true);
     if ($method != 'bitcoin_private')
         return;
 
-    $paymentAddress = get_post_meta($order->id, 'paymentAddress', true);
-    $paymentTxid = get_post_meta($order->id, 'paymentTxid', true);
-    $paymentTxref = get_post_meta($order->id, 'paymentTxref', true);
+    $payment_address = sanitize_text_field(get_post_meta($order->id, 'btcp_payment_address', true));
+    $payment_txid = intval(get_post_meta($order->id, 'btcp_payment_txid', true));
+    $payment_txref = sanitize_text_field(get_post_meta($order->id, 'btcp_payment_txref', true));
 
-    echo '<p><strong>' . __('Payment Address') . ':</strong> <a href="https://explorer.btcprivate.org/address/' . $paymentAddress . '" target="_blank"> ' . $paymentAddress . '</a></p>';
-    echo '<p><strong>' . __('BTCP Transaction ID') . ':</strong> <a href="https://explorer.btcprivate.org/tx/' . $paymentTxid . '" target="_blank"> ' . $paymentTxid . '</a></p>';
-    echo '<p><strong>' . __('BTCP Pay TransactionRef') . ':</strong> ' . $paymentTxref . '</p>';
+    echo '<p><strong>' . __('Payment Address') . ':</strong> <a href="https://explorer.btcprivate.org/address/' . $payment_address . '" target="_blank"> ' . $payment_address . '</a></p>';
+    echo '<p><strong>' . __('BTCP Transaction ID') . ':</strong> <a href="https://explorer.btcprivate.org/tx/' . $payment_txid . '" target="_blank"> ' . $payment_txid . '</a></p>';
+    echo '<p><strong>' . __('BTCP Pay TransactionRef') . ':</strong> ' . $payment_txref . '</p>';
 
 }
-
